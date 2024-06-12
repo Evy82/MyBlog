828bf7895bccb93dfa83ed99099229eeb2a910c0
PatriciaDev <109634022+Evy82@users.noreply.github.com>
2024-06-12 12:53:43 +0200

    improve readability

--- models/post.js
+++ models/post.js
@@ -1,41 +1,53 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to DB'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+const databaseConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB');
+  } catch (err) {
+    console.error('Could not connect to MongoDB...', err);
+  }
+};
+
+databaseConnection();
 
 const blogPostSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'Title is required']
+    required: [true, 'Title is required'],
   },
   author: {
     type: String,
-    required: [true, 'Author is required']
+    required: [true, 'Author is required'],
   },
   body: {
     type: String,
-    required: [true, 'Body text is required']
+    required: [true, 'Body text is required'],
   },
-  comments: [{
-    body: String,
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  }],
+  comments: [
+    {
+      body: String,
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   hidden: Boolean,
   meta: {
     votes: Number,
-    favs: Number
-  }
+    favs: Number,
+  },
 });
 
-blogPostSchema.post('save', function(error, doc, next) {
+blogPostSchema.post('save', (error, doc, next) => {
   if (error.name === 'MongoError' && error.code === 11000) {
     next(new Error('There was a duplicate key error'));
   } else if (error) {
@@ -47,11 +59,15 @@ blogPostSchema.post('save', function(error, doc, next) {
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-module.exports = BlogPost;
+const saveNewBlogPost = async (blogPostDetails) => {
+  const newBlogPost = new BlogPost(blogFeaturingPostDetails);
 
-const newBlogPost = new BlogPost({
-});
+  try {
+    const doc = await newBlogPost.save();
+    console.log('Blog post saved:', doc);
+  } catch (err) {
+    console.error('Error saving the blog post:', err);
+  }
+};
 
-newBlogItPost.save()
-  .then(doc => console.log('Blog post saved:', doc))
-  .catch(err => console.error('Error saving the blog post:', err));
+module.exports = { BlogPost, saveNewBlogPost };
